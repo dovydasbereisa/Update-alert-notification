c2e8bb3b8053ec591816ea0ce43183f4409a6fde
Dovydas Bere <dovydasbereisa@gmail.com>
2018-04-09 22:06:24 +0300

    Add files via upload

---
+++ UPDATE_CHECKERTests/UPDATE_CHECKERTests.swift
@@ -0,0 +1,36 @@
+//
+//  UPDATE_CHECKERTests.swift
+//  UPDATE_CHECKERTests
+//
+//  Created by Dovydas Bereisa on 4/3/18.
+//  Copyright © 2018 Dovydas Bereisa. All rights reserved.
+//
+
+import XCTest
+@testable import UPDATE_CHECKER
+
+class UPDATE_CHECKERTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
